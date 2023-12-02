b1b75accd91f02764428d9de3eab634049785309
stefanbalta <stefan.balta.sb@gmail.com>
2023-12-01 20:54:52 -0500

    perf(day 1): improve efficiency of day one part two solution

--- day1.py
+++ day1.py
@@ -20,30 +20,24 @@ def day1_part_1():
         sum += firstNumber * 10 + secondNumber
     print(sum)
 
-def check_if_num_in_string(string: str) -> Optional[int]:
-    numberDict: dict[str,int] = {
-            "one": 1,
-            "two": 2,
-            "three": 3,
-            "four": 4,
-            "five":5,
-            "six": 6,
-            "seven": 7,
-            "eight": 8,
-            "nine": 9,
-            }
-    if len(string) >=3:
-        if string[0:3] in numberDict:
-            return numberDict[string[0:3]]
-        if string[0:4] in numberDict:
-            return numberDict[string[0:4]]
-        if string[0:5] in numberDict:
-            return numberDict[string[0:5]]
-
-    return None
-
 def day1_part_2():
-    parsed_input: list[str] = day1_parser(open("day1.txt", "r").read())
+    numberWords: dict[str, int] = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five":5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
+    }
+    initial_input: str = open("day1.txt", "r").read()
+    for word in numberWords.keys():
+        initial_input = initial_input.replace(word, word[0] + word + word[-1])
+    for word in numberWords.keys():
+        initial_input = initial_input.replace(word, str(numberWords[word]))
+    parsed_input: list[str] = day1_parser(initial_input)
     sum: int = 0
     for line in parsed_input:
         if line == "":
@@ -58,11 +52,6 @@ def day1_part_2():
                     firstNumber = currentNum
                 secondNumber = currentNum
             except:
-                currentNum: Optional[int] = check_if_num_in_string(line[i:min(i+5, len(line))])
-                if currentNum:
-                    if firstNumber == None:
-                        firstNumber = currentNum
-                    secondNumber = currentNum
                 pass
         sum += firstNumber * 10 + secondNumber
     print(sum)
