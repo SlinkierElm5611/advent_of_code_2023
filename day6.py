13f70b77fcb5d52bdea1b0c6c0dce0fbb0e97473
SlinkierElm5611 <stefan.balta.sb@gmail.com>
2023-12-06 09:01:50 -0500

    Optimized day 6

--- day6.py
+++ day6.py
@@ -1,3 +1,4 @@
+import math
 def day6_part_1():
     input_file = open("day6.txt", "r")
     input_lines = input_file.readlines()
@@ -6,12 +7,14 @@ def day6_part_1():
     distances: list[int] = [int(distance) for distance in input_lines[1].split(":")[1].strip().split(" ") if distance != ""]
     product: int = 1
     for race_index in range(len(times)):
-        sum = 0
-        for t in range(times[race_index]+1):
-            if t*(times[race_index] - t) > distances[race_index]:
-                sum += 1
-        if sum >=1:
-            product *= sum
+        distance = distances[race_index]
+        time = times[race_index]
+        discriminant = time * time - 4 * distance
+        min_x = math.ceil((time - discriminant ** 0.5) / 2)
+        max_x = math.floor((time + discriminant ** 0.5) / 2)
+        min_x = min_x + 1 if time * min_x - min_x*min_x <= distance else min_x 
+        max_x = max_x - 1 if time * max_x -max_x*max_x<= distance else max_x
+        product *= max_x - min_x + 1
     print(product)
 
 def day6_part_2():
@@ -20,8 +23,9 @@ def day6_part_2():
     input_file.close()
     time: int= int("".join([time for time in input_lines[0].split(":")[1].strip().split(" ") if time != ""]))
     distance: int = int("".join([distance for distance in input_lines[1].split(":")[1].strip().split(" ") if distance != ""]))
-    sum = 0
-    for t in range(time+1):
-        if t*(time - t) > distance:
-            sum += 1
-    print(sum)
+    discriminant = time * time - 4 * distance
+    min_x = math.ceil((time - discriminant ** 0.5) / 2)
+    max_x = math.floor((time + discriminant ** 0.5) / 2)
+    min_x = min_x + 1 if time * min_x - min_x*min_x <= distance else min_x 
+    max_x = max_x - 1 if time * max_x -max_x*max_x<= distance else max_x
+    print(max_x - min_x + 1)
